8f4520486a3f57eab246e24245fe70a3e010c616
sunxyw <xy2496419818@gmail.com>
2018-02-12 18:32:03 +0800

    增加加载效果

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,26 @@
 $(document).ready(function () {
+    var load = 0;
     window.onload = function () {
-        $('.loader').fadeOut('slow');
+        $(".loader b").fadeOut();
+        setTimeout(function () {
+            $(".loader b").text("加载完成 点我继续");
+            $(".loader").css("cursor", "pointer");
+        }, 500);
+        $(".loader b").fadeIn("slow");
+        load = 1;
     };
 
-    $('.nav-link').mouseenter(function () {
-        $(this).addClass('link-active');
-    })
+    $(".loader").click(function () {
+        if (load = 1) {
+            $(".loader").fadeOut("slow");
+        }
+    });
 
-    $('.nav-link').mouseleave(function () {
-        $(this).removeClass('link-active');
-    })
+    $(".nav-link").mouseenter(function () {
+        $(this).addClass("link-active");
+    });
+
+    $(".nav-link").mouseleave(function () {
+        $(this).removeClass("link-active");
+    });
 });
