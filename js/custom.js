4ab695335419584ad96d4534676c1c28fba2bd05
sunxyw <xy2496419818@gmail.com>
2018-02-12 17:59:42 +0800

    增加导航栏特效

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
     window.onload = function () {
         $('.loader').fadeOut('slow');
     };
+
+    $('.nav-link').mouseenter(function () {
+        $(this).addClass('link-active');
+    })
+
+    $('.nav-link').mouseleave(function () {
+        $(this).removeClass('link-active');
+    })
 });
